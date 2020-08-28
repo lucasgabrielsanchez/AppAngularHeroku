8eb15eeb1bfaf751a3437cfb351bcd3dc746969d
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2020-08-27 22:06:12 -0300

    Clase 1 ejercicio.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'projectClase1';
+  edad1: number;
+  edad2: number;
+
+  resultadoSuma: number;
+  resultadoPromedio: number;
+
+  calcular() {
+    this.resultadoSuma = Number(this.edad1) + Number(this.edad2);
+    this.resultadoPromedio = this.resultadoSuma / 2;
+  }
+
+  borrar() {
+    this.edad1 = null;
+    this.edad2 = null;
+    this.resultadoSuma = null;
+    this.resultadoPromedio = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, BienvenidaComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
